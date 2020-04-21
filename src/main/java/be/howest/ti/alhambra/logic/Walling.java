e845778fc8ba69973052de31068212c3c6e1ff2a
Laurens Giesen <laurens.giesen@student.howest.be>
2020-04-21 15:17:42 +0200

    add constructor to Walling

--- src/main/java/be/howest/ti/alhambra/logic/Walling.java
+++ src/main/java/be/howest/ti/alhambra/logic/Walling.java
@@ -7,4 +7,10 @@ public class Walling {
     
     private Map<WallingDirection, Boolean> walls = new HashMap<>();
 
+    public Walling(boolean north, boolean east, boolean south, boolean west) {
+        this.walls.put(WallingDirection.NORTH, north);
+        this.walls.put(WallingDirection.EAST, east);
+        this.walls.put(WallingDirection.SOUTH, south);
+        this.walls.put(WallingDirection.WEST, west);
+    }
 }
