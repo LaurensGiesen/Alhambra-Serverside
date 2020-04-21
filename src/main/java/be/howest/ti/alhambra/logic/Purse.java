cec670e85ea9127f164f7fc390b59308e9fc6af3
michi <michiel.smalle@student.howest.be>
2020-04-21 14:18:48 +0200

    Class Purse finalisatie done

--- src/main/java/be/howest/ti/alhambra/logic/Purse.java
+++ src/main/java/be/howest/ti/alhambra/logic/Purse.java
@@ -2,20 +2,41 @@ package be.howest.ti.alhambra.logic;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import be.howest.ti.alhambra.logic.Coin;
-import be.howest.ti.alhambra.logic.Currency;
+import be.howest.ti.alhambra.logic.exceptions.AlhambraEntityNotFoundException;
+import be.howest.ti.alhambra.logic.sort.SortByCoin;
 
 public class Purse {
-    private int totalAmount;
-    private Currency currency;
+
+    public List<Coin> coins;
+
+    public Purse() {
+        coins = new ArrayList<>();
+    }
+
+    public boolean addCoin(Coin coin) {
+        return coins.add(coin);
+    }
+
+    public boolean removeCoin(Coin coin) {
+        return coins.remove(coin);
+    }
+
+    public List<Coin> getCoins() {
+
+        coins.sort(new SortByCoin());
+        return coins;
+
+    }
 
     public int getTotalAmount() {
-        return totalAmount;
+        return coins.stream().map(Coin::getAmount).reduce(0, Integer::sum);
     }
 
     public Currency getCurrency() {
-        return currency;
+        if (coins.stream().allMatch(c -> c.getCurrency().equals(coins.get(0).getCurrency()))) {
+            return coins.get(0).getCurrency();
+        }else {
+            throw new AlhambraEntityNotFoundException("exceptional situation");
+        }
     }
-
 }
---
+++ src/main/java/be/howest/ti/alhambra/logic/sort/SortByCoin.java
@@ -0,0 +1,19 @@
+package be.howest.ti.alhambra.logic.sort;
+
+import java.util.Comparator;
+import be.howest.ti.alhambra.logic.Coin;
+
+public class SortByCoin implements Comparator<Coin> {
+
+        public int compare(Coin o1, Coin o2) {
+
+            int compare;
+            compare = o1.getCurrency().compareTo(o2.getCurrency());
+
+            if (compare != 0) {
+                return compare;
+            }
+            return o1.getAmount() - o2.getAmount();
+
+        }
+}
