3bd48bff7c9abea4a97d3990ee724c2aa7b3623f
michi <michiel.smalle@student.howest.be>
2020-04-21 11:50:44 +0200

    Class Purse aanmaken done

--- src/main/java/be/howest/ti/alhambra/logic/Purse.java
+++ src/main/java/be/howest/ti/alhambra/logic/Purse.java
@@ -1,9 +1,21 @@
 package be.howest.ti.alhambra.logic;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import be.howest.ti.alhambra.logic.Coin;
+import be.howest.ti.alhambra.logic.Currency;
 
 public class Purse {
-    ArrayList<String> Coins = new ArrayList<String>();
+    private int totalAmount;
+    private Currency currency;
+
+    public int getTotalAmount() {
+        return totalAmount;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
 }
