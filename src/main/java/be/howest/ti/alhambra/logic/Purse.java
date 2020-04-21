11f6ff5c94a58e27b8ec45a10e91b45f847a0d37
michi <michiel.smalle@student.howest.be>
2020-04-21 11:26:09 +0200

    Init commit Purse

---
+++ src/main/java/be/howest/ti/alhambra/logic/Purse.java
@@ -0,0 +1,9 @@
+package be.howest.ti.alhambra.logic;
+
+import java.util.ArrayList;
+
+import be.howest.ti.alhambra.logic.Coin;
+
+public class Purse {
+    ArrayList<String> Coins = new ArrayList<String>();
+}
