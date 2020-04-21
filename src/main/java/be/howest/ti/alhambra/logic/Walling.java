42c68494e1fa0ded7054301643fded07d33c9eef
Laurens Giesen <laurens.giesen@student.howest.be>
2020-04-21 16:35:49 +0200

    Add constructor to Class Walling

--- src/main/java/be/howest/ti/alhambra/logic/Walling.java
+++ src/main/java/be/howest/ti/alhambra/logic/Walling.java
@@ -2,7 +2,7 @@ package be.howest.ti.alhambra.logic;
 
 import java.util.HashMap;
 import java.util.Map;
-import be.howest.ti.alhambra.logic.WallingDirection;
+
 public class Walling {
     
     private Map<WallingDirection, Boolean> walls = new HashMap<>();
