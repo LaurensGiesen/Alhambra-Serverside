d957c12358e347c337d66dbe352c9417d5a49bdf
Laurens Giesen <laurens.giesen@student.howest.be>
2020-04-24 23:08:56 +0200

    Test addBuilding and removeBuilding -->  BuildingPlace(class)

--- src/test/java/be/howest/ti/alhambra/logic/BuildingPlaceTest.java
+++ src/test/java/be/howest/ti/alhambra/logic/BuildingPlaceTest.java
@@ -2,18 +2,27 @@ package be.howest.ti.alhambra.logic;
 
 import be.howest.ti.alhambra.logic.building.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class BuildingPlaceTest {
 
-    @Test
+    @Test //Test for addBuilding and removeBuilding
     void BuildingPlace() {
         Walling w = new Walling(true, false, false, true);
         Building b = new Building(Buildingtype.TOWER, 10,w);
     BuildingPlace bp = new BuildingPlace();
     bp.addBuilding(b);
-    
+    assertTrue(bp.getBuildings().contains(b));
+    bp.removeBuilding(b);
+    List<Building> buildings;
+    buildings = new ArrayList<>();
+    assertEquals(bp.getBuildings(), buildings);
     }
+
 
 }
