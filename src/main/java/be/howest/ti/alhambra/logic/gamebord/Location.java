bc18225d3bebfe024271e6012c7578c2826f9a6a
kwinten <kwinten.tas@student.howest.be>
2020-04-24 16:20:42 +0200

    add equals & hashcode to Location

--- src/main/java/be/howest/ti/alhambra/logic/gamebord/Location.java
+++ src/main/java/be/howest/ti/alhambra/logic/gamebord/Location.java
@@ -2,6 +2,8 @@ package be.howest.ti.alhambra.logic.gamebord;
 
 import be.howest.ti.alhambra.logic.building.WallingDirection;
 
+import java.util.Objects;
+
 public class Location {
     private int row;
     private int col;
@@ -31,5 +33,19 @@ public class Location {
             default:
                 return new Location(row, col - 1);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return row == location.row &&
+                col == location.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
     }
 }
