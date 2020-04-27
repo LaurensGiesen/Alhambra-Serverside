fce9238aab103acd97d871fc4bc0153a51154627
Laurens Giesen <laurens.giesen@student.howest.be>
2020-04-27 16:59:07 +0200

    tested exception of removeBuilding(last test)

--- src/test/java/be/howest/ti/alhambra/logic/BuildingPlaceTest.java
+++ src/test/java/be/howest/ti/alhambra/logic/BuildingPlaceTest.java
@@ -1,6 +1,7 @@
 package be.howest.ti.alhambra.logic;
 
 import be.howest.ti.alhambra.logic.building.*;
+import be.howest.ti.alhambra.logic.exceptions.AlhambraEntityNotFoundException;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -10,18 +11,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class BuildingPlaceTest {
-
-    @Test //Test for addBuilding and removeBuilding
+    Walling w1;
+    Building b1;
+    Building b2;
+    BuildingPlace bp;
+public void createBuildingPlace() {
+    w1 = new Walling(true, false, false, true);
+    b1 = new Building(Buildingtype.TOWER, 10,w1);
+    bp = new BuildingPlace();
+    bp.addBuilding(b1);
+}
+    @Test
     void BuildingPlace() {
-        Walling w = new Walling(true, false, false, true);
-        Building b = new Building(Buildingtype.TOWER, 10,w);
-    BuildingPlace bp = new BuildingPlace();
-    bp.addBuilding(b);
-    assertTrue(bp.getBuildings().contains(b));
-    bp.removeBuilding(b);
+    createBuildingPlace();
+    assertTrue(bp.getBuildings().contains(b1));
+    bp.removeBuilding(b1);
     List<Building> buildings;
     buildings = new ArrayList<>();
     assertEquals(bp.getBuildings(), buildings);
+    assertThrows(AlhambraEntityNotFoundException.class, () -> bp.removeBuilding(b2));
     }
 
 
