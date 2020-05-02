2202e32c8ccec0fe0656247de89beff00792e53e
kwinten <kwinten.tas@student.howest.be>
2020-05-02 18:29:23 +0200

    test commit

--- src/test/java/be/howest/ti/alhambra/logic/Game/GameTest.java
+++ src/test/java/be/howest/ti/alhambra/logic/Game/GameTest.java
@@ -1,5 +1,6 @@
 package be.howest.ti.alhambra.logic.Game;
 
+import be.howest.ti.alhambra.logic.exceptions.AlhambraEntityNotFoundException;
 import be.howest.ti.alhambra.logic.exceptions.AlhambraGameRuleException;
 import be.howest.ti.alhambra.logic.gamebord.Player;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,8 +10,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
-    Game g1;
-    Game g2;
+    Game g1; // 2 player game;
+    Game g2; //empty game
 
     @BeforeEach
     private void createGame(){
@@ -32,5 +33,15 @@ class GameTest {
         g2.addPlayer("E");
         g2.addPlayer("F");
         assertThrows(AlhambraGameRuleException.class, ()->g2.addPlayer("G")); //Max 6 players
+    }
+
+
+    @Test
+    void removePlayer() {
+        assertDoesNotThrow(()->g1.removePlayer("A"));
+        assertNull(g1.getPlayerByName("A"));
+
+        assertThrows(AlhambraEntityNotFoundException.class, ()->g1.removePlayer("D")); //Player not present
+                      
     }
 }
