e619862c7a1e8a25dc5b004b14bec60c8bbe73d3
Laurens Giesen <laurens.giesen@student.howest.be>
2020-05-04 14:50:20 +0200

    commit for pull

--- src/main/java/be/howest/ti/alhambra/logic/StartUp.java
+++ src/main/java/be/howest/ti/alhambra/logic/StartUp.java
@@ -1,22 +1,11 @@
 package be.howest.ti.alhambra.logic;
 
-import be.howest.ti.alhambra.logic.coin.Coin;
-import be.howest.ti.alhambra.logic.coin.Currency;
-import be.howest.ti.alhambra.logic.coin.Purse;
-
-import static be.howest.ti.alhambra.logic.coin.Currency.BLUE;
-
-
 public class StartUp {
     public static void main(String[] args) {
         new StartUp().run();
     }
 
     private void run() {
-        Coin c5 = new Coin(BLUE, 12);
-        Purse p3 = new Purse();
-        p3.addCoin(c5);
-        System.out.println(c5.getCurrency());
 
     }
 }
