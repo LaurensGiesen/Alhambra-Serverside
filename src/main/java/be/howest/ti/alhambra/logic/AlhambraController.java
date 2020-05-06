53e304deac0481eed4855d45211e697ecd9c0fbc
kwinten <kwinten.tas@student.howest.be>
2020-05-06 17:56:30 +0200

    implement function createGame

--- src/main/java/be/howest/ti/alhambra/logic/AlhambraController.java
+++ src/main/java/be/howest/ti/alhambra/logic/AlhambraController.java
@@ -1,9 +1,21 @@
 package be.howest.ti.alhambra.logic;
 
+import be.howest.ti.alhambra.logic.Game.Server;
 import be.howest.ti.alhambra.logic.coin.Currency;
 
 public class AlhambraController {
+
+    Server server;
+
+    public AlhambraController() {
+        server = new Server();
+    }
+
     public Currency[] getCurrencies() {
         return Currency.values();
+    }
+
+    public String createGame() {
+        return "" + server.newGame();
     }
 }
