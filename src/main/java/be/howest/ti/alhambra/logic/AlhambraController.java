2623243518eaa256dda079e5eb9f92dd393ddd65
kwinten <kwinten.tas@student.howest.be>
2020-05-06 17:26:51 +0200

    implement function getCurrencies

--- src/main/java/be/howest/ti/alhambra/logic/AlhambraController.java
+++ src/main/java/be/howest/ti/alhambra/logic/AlhambraController.java
@@ -3,5 +3,7 @@ package be.howest.ti.alhambra.logic;
 import be.howest.ti.alhambra.logic.coin.Currency;
 
 public class AlhambraController {
-
+    public Currency[] getCurrencies() {
+        return Currency.values();
+    }
 }
