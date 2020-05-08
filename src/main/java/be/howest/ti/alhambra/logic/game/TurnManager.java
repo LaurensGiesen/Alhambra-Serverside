45d2420162e12c03cfe9211b251b05398d25d2e5
kwinten <kwinten.tas@student.howest.be>
2020-05-08 17:16:16 +0200

    Add end of game to TurnManager add test for endOfGame in TurnManager

--- src/main/java/be/howest/ti/alhambra/logic/game/TurnManager.java
+++ src/main/java/be/howest/ti/alhambra/logic/game/TurnManager.java
@@ -4,21 +4,29 @@ import be.howest.ti.alhambra.logic.gamebord.Player;
 
 public class TurnManager {
 
-    private TurnManager() {}
+    private TurnManager() {
+    }
 
-    public static boolean isReadyToStart(Game game){
+    public static boolean isReadyToStart(Game game) {
         return !game.isStarted() && game.getPlayerCount() >= 2 && game.getPlayerCount() == game.getReadyCount();
     }
 
-    public static void startGame(Game game){
+    public static void startGame(Game game) {
         Populator.populateBank(game.getCoinStack(), game.getBank());
         Populator.populateMarket(game.getBuildingStack(), game.getMarket());
-        for(Player p : game.getPlayers()){
+        for (Player p : game.getPlayers()) {
             Populator.giveStartMoney(game.getCoinStack(), p.getMoney());
         }
         determineStarter(game);
     }
 
+    public static void endTurn(Game game) {
+        Populator.populateBank(game.getCoinStack(), game.getBank());
+        Populator.populateMarket(game.getBuildingStack(), game.getMarket());
+        setNextPlayer(game);
+    }
+
+    
     private static void determineStarter(Game game) {
         //get player with minimum cards
         //if equal, get player with min value
@@ -35,6 +43,17 @@ public class TurnManager {
                 game.setCurrentPlayer(p);
                 totalAmountOfCoins = p.getMoney().getTotalAmount();
             }
+        }
+    }
+
+    private static void setNextPlayer(Game game) {
+        Player currPlayer = game.getCurrentPlayer();
+        int posCurrPlayer = game.getPlayers().indexOf(currPlayer);
+
+        if (posCurrPlayer == game.getPlayers().size() - 1) {
+            game.setCurrentPlayer(game.getPlayers().get(0));
+        } else {
+            game.setCurrentPlayer(game.getPlayers().get(posCurrPlayer + 1));
         }
     }
 
--- src/test/java/be/howest/ti/alhambra/logic/game/TurnManagerTest.java
+++ src/test/java/be/howest/ti/alhambra/logic/game/TurnManagerTest.java
@@ -1,6 +1,7 @@
 package be.howest.ti.alhambra.logic.game;
 
 import be.howest.ti.alhambra.logic.coin.Currency;
+import be.howest.ti.alhambra.logic.gamebord.Player;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,13 +10,19 @@ class TurnManagerTest {
 
     Game g1;
 
-    private void getGameReadyToStart(){
+    private void getGameReadyToStart() {
         g1 = new Game();
         g1.addPlayer("A");
         g1.addPlayer("B");
         g1.getPlayerByName("A").setReady(true);
         g1.getPlayerByName("B").setReady(true);
     }
+
+    private void getStartedGame() {
+        getGameReadyToStart();
+        TurnManager.startGame(g1);
+    }
+
 
     @Test
     void isReadyToStart() {
@@ -34,7 +41,7 @@ class TurnManagerTest {
     void startGame() {
         getGameReadyToStart();
 
-        assertDoesNotThrow(()->TurnManager.startGame(g1));
+        assertDoesNotThrow(() -> TurnManager.startGame(g1));
         assertEquals(4, g1.getBank().getCoins().size());
         assertNotNull(g1.getMarket().get(Currency.BLUE));
         assertNotNull(g1.getMarket().get(Currency.ORANGE));
@@ -43,5 +50,20 @@ class TurnManagerTest {
         assertNotNull(g1.getCurrentPlayer());
         assertTrue(g1.getPlayerByName("A").getMoney().getNumberOfCoins() > 0);
         assertTrue(g1.getPlayerByName("A").getMoney().getTotalAmount() > 20);
+    }
+
+    @Test
+    void endTurn() {
+        getStartedGame();
+        Player currPlayer = g1.getCurrentPlayer();
+        assertDoesNotThrow(() -> TurnManager.endTurn(g1));
+        assertNotEquals(currPlayer, g1.getCurrentPlayer());
+        TurnManager.endTurn(g1);
+        assertEquals(currPlayer, g1.getCurrentPlayer());
+        assertEquals(4, g1.getBank().getNumberOfCoins());
+        assertNotNull(g1.getMarket().get(Currency.BLUE));
+        assertNotNull(g1.getMarket().get(Currency.YELLOW));
+        assertNotNull(g1.getMarket().get(Currency.ORANGE));
+        assertNotNull(g1.getMarket().get(Currency.GREEN));
     }
 }
