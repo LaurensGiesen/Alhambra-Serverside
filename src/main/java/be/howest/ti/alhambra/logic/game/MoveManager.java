f9804d8e3620522891330f367dd76e8de40ceb34
Quinten vandekerckhove <quinten.vandekerckhove@student.howest.be>
2020-05-09 14:21:17 +0200

    Class MoveManager fix

--- src/main/java/be/howest/ti/alhambra/logic/game/MoveManager.java
+++ src/main/java/be/howest/ti/alhambra/logic/game/MoveManager.java
@@ -16,7 +16,7 @@ public class MoveManager {
         }
     }
 
-    public boolean canTakeMoney(Game game, Player player){
+    public boolean canTakeMoney(Game game, Player player, Purse coinsToTake){
         canPlay(game, player);
 
         if (game.getPlayerByName(player.getPlayerName()) == null) {
@@ -25,7 +25,7 @@ public class MoveManager {
         if (game.getPlayerByName(player.getPlayerName()) != game.getCurrentPlayer()) {
             throw new AlhambraGameRuleException("Not the current player");
         }
-        for (Coin coin : game.getBank().getCoins()) {
+        for (Coin coin : coinsToTake.getCoins()) {
             if (!game.getBank().getCoins().contains(coin)) {
                 throw new AlhambraEntityNotFoundException("No such coin in Bank");
             }
--- src/test/java/be/howest/ti/alhambra/logic/game/MoveManagerTest.java
+++ src/test/java/be/howest/ti/alhambra/logic/game/MoveManagerTest.java
@@ -1,5 +1,11 @@
 package be.howest.ti.alhambra.logic.game;
 
+import be.howest.ti.alhambra.logic.coin.Coin;
+import be.howest.ti.alhambra.logic.coin.Currency;
+import be.howest.ti.alhambra.logic.coin.Purse;
+import be.howest.ti.alhambra.logic.exceptions.AlhambraEntityNotFoundException;
+import be.howest.ti.alhambra.logic.exceptions.AlhambraGameRuleException;
+import be.howest.ti.alhambra.logic.gamebord.Player;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,8 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MoveManagerTest {
 
     @Test
-    void canTakeMoney() {
-        
-    }
+    void takeMoney() {
 
+    }
 }
