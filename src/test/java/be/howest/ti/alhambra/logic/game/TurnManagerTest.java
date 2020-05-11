176b90e85c0955db34d377337919f4af4d72905e
Jonas Herteleer <jonas.herteleer@student.howest.be>
2020-05-11 14:08:46 +0200

    CurrentPlayer changed to String

--- src/test/java/be/howest/ti/alhambra/logic/game/TurnManagerTest.java
+++ src/test/java/be/howest/ti/alhambra/logic/game/TurnManagerTest.java
@@ -55,7 +55,7 @@ class TurnManagerTest {
     @Test
     void endTurn() {
         getStartedGame();
-        Player currPlayer = g1.getCurrentPlayer();
+        String currPlayer = g1.getCurrentPlayer();
         assertDoesNotThrow(() -> TurnManager.endTurn(g1));
         assertNotEquals(currPlayer, g1.getCurrentPlayer());
         TurnManager.endTurn(g1);
